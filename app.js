f651b9e2db0cd282a7d37ada159667304eeaa4c1
Miguel Rincon <miguel@baune.io>
2024-11-10 10:47:16 -0500

    * changes

--- app.js
+++ app.js
@@ -35,4 +35,69 @@ app.get('/waste', (req, res) => {
 
 });
 
+app.post('/signin', (req, res) => {
+    let email = req.body.email
+    console.log(email)
+
+    // If pantry exists return contents to frontend, if it doesn't, create empty pantry and send to frontend
+    const fs = require('fs');
+
+    try {
+        let path = `./pantries/${email}.json`
+        if (fs.existsSync(path)) {
+            console.log("Pantry exists!")
+            // TODO return JSON
+
+            // Read the JSON file
+            fs.readFile(path, 'utf8', (err, data) => {
+                if (err) {
+                    console.error('Error reading file:', err);
+                    return res.status(500).json({ error: 'Internal Server Error' });
+                }
+
+                // Parse the JSON data and send it as response
+                try {
+                    const jsonData = JSON.parse(data);
+                    res.json(jsonData);
+                } catch (parseErr) {
+                    console.error('Error parsing JSON:', parseErr);
+                    res.status(500).json({ error: 'Invalid JSON format' });
+                }
+            });
+
+        } else {
+            console.log("Pantry does not exist!")
+            // TODO Create empty JSON and return
+            let newPantry = {}
+
+            fs.writeFile(path, JSON.stringify(newPantry), (err) => {
+                if (err) {
+                    console.error("error writing to file", err)
+                } else {
+                    console.log("JSON file has been created!")
+                }
+            })
+
+            // Read the JSON file
+            fs.readFile(path, 'utf8', (err, data) => {
+                if (err) {
+                    console.error('Error reading file:', err);
+                    return res.status(500).json({ error: 'Internal Server Error' });
+                }
+
+                // Parse the JSON data and send it as response
+                try {
+                    const jsonData = JSON.parse(data);
+                    res.json(jsonData);
+                } catch (parseErr) {
+                    console.error('Error parsing JSON:', parseErr);
+                    res.status(500).json({ error: 'Invalid JSON format' });
+                }
+            });
+        }
+    } catch (err) {
+        console.log(err)
+    }
+})
+
 module.exports = app;
